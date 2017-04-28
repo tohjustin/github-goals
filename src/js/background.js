4f93b4eb0690a33167c319e92dcc3baaead96418
Justin Toh <tohjustin@hotmail.com>
2017-04-27 21:35:06 -0700

    Got updating badges to work

--- src/js/background.js
+++ src/js/background.js
@@ -1,11 +1,5 @@
- // Checks every 1 second
-const INTERVAL = 1;
-
-function helloWorld() {
-  let x = 0;
-  setInterval(() => {
-    console.log(`hello world! (time elapsed: ${x += 1}s)`); // eslint-disable-line no-console
-  }, INTERVAL * 1000);
+function setBadgeText(text) {
+  chrome.browserAction.setBadgeText({ text });
 }
 
-helloWorld();
+setBadgeText('21');
