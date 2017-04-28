b9fa1498964990787d59ba6835488afb93efe529
Justin Toh <tohjustin@hotmail.com>
2017-04-27 22:59:02 -0700

    Managed to get basic functionality working

--- src/js/background.js
+++ src/js/background.js
@@ -1,15 +1,31 @@
 import contributions from './modules/git-contribution';
 
-function setBadgeText(text) {
-  chrome.browserAction.setBadgeText({ text });
-}
+// Scraps & update commit count every 'INTERVAL' amount of seconds
+const INTERVAL = 30;
 
-contributions.daily('tohjustin')
-  .then((commitCount) => {
-    console.log(commitCount); // eslint-disable-line no-console
-    setBadgeText(commitCount.toString());
-  })
-  .catch((err) => {
-    setBadgeText('X');
-    console.log('ERROR', err); // eslint-disable-line no-console
-  });
+const setBadgeText = (text) => {
+  chrome.browserAction.setBadgeText({ text });
+};
+
+const setBadgeBackgroundColor = (color) => {
+  chrome.browserAction.setBadgeBackgroundColor({ color });
+};
+
+const retrieveDailyCommitCount = (username) => {
+  contributions.daily(username)
+    .then((commitCount) => {
+      console.log(commitCount); // eslint-disable-line no-console
+      setBadgeText(commitCount.toString());
+    })
+    .catch((err) => {
+      setBadgeText('X');
+      console.log('ERROR', err); // eslint-disable-line no-console
+    });
+};
+
+setInterval(() => {
+  retrieveDailyCommitCount('tohjustin');
+}, INTERVAL * 1000);
+
+setBadgeBackgroundColor('#F34336');
+retrieveDailyCommitCount('tohjustin');
