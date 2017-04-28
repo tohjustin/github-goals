7ecbdfcbce289a82795d04f657db1ee344da1439
Justin Toh <tohjustin@hotmail.com>
2017-04-28 12:10:03 -0700

    Refactored code & enabled ESlinting on modules/git-contribution.js

--- src/js/modules/git-contribution.js
+++ src/js/modules/git-contribution.js
@@ -1,60 +1,51 @@
-/* eslint-disable */
 import https from 'https';
 import cheerio from 'cheerio';
 import moment from 'moment';
 
-var body;
-var $;
-var days;
-
 const contributions = {};
 
-var getDays = function(username) {
-	return new Promise(function(resolve, reject) {
-		var url;
-		if (username) {
-			url = `https://github.com/users/${username}/contributions`;
-		}
-		else {
-			reject('No username provided');
-		}
-		https.get(url, function(res) {
-			res.setEncoding('utf8');
-			res.on('data', function(data) {
-				body += data;
-			})
+const getDays = username =>
+  new Promise((resolve, reject) => {
+    const url = `https://github.com/users/${username}/contributions`;
+    let body;
 
-			if (res.statusCode !== 200) {
-				reject(res.statusCode)
-			}
+    https.get(url, (res) => {
+      res.setEncoding('utf8');
+      res.on('data', (data) => {
+        body += data;
+      });
 
-			res.on('end', function() {
-				$ = cheerio.load(body, { ignoreWhitespace: true, decodeEntities: true });
-				var weeks = $('g', 'g');
-				days = $('rect', weeks);
-				resolve(days);
-			})
-		}).on('error', function(err) {
-			reject(err);
-		})
-	})
-}
+      if (res.statusCode !== 200) {
+        reject(res.statusCode);
+      }
 
-contributions.daily = function(username) {
-	return new Promise((resolve, reject) => {
-		getDays(username).then((days) => {
-			const todayDateStr = moment().format('YYYY-MM-DD');
-			const index = days.length-1;
-			let commitCount = parseInt($(days[index]).attr('data-count'));
+      res.on('end', () => {
+        const $ = cheerio.load(body, { ignoreWhitespace: true, decodeEntities: true });
+        const days = $('rect[class=day]').map((index, e) => {
+          const { attribs: { 'data-count': count, 'data-date': date } } = e;
+          return { count, date };
+        });
+        resolve(days);
+      });
+    }).on('error', (err) => {
+      reject(err);
+    });
+  });
 
-			// If latest entry in Github's commit graph isn't the current browser's date,
-			// we also include the commits from previous day.
-			commitCount = (days[index].attribs['data-date'] === todayDateStr) ? commitCount :
-				commitCount + parseInt($(days[index-1]).attr('data-count'));
+contributions.daily = username =>
+  new Promise((resolve) => {
+    getDays(username).then((days) => {
+      const todayDateStr = moment().format('YYYY-MM-DD');
+      const index = days.length - 1;
+      let commitCount = parseInt(days[index].count, 10);
 
-			resolve(commitCount);
-		});
-	})
-}
+      // If latest entry in Github's commit graph isn't the current browser's date,
+      // we also include the commits from previous day.
+      commitCount = (days[index].date === todayDateStr) ? commitCount :
+        commitCount + parseInt(days[index - 1].count, 10);
+
+      resolve(commitCount);
+    });
+  });
 
 export default contributions;
