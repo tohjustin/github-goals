a9c0d0492a664993865e842280328d2fddbf1fdd
Justin Toh <tohjustin@hotmail.com>
2017-04-27 22:39:37 -0700

    Managed to retrieve commit count from github.com

--- src/js/background.js
+++ src/js/background.js
@@ -1,5 +1,15 @@
+import contributions from './modules/git-contribution';
+
 function setBadgeText(text) {
   chrome.browserAction.setBadgeText({ text });
 }
 
-setBadgeText('21');
+contributions.daily('tohjustin')
+  .then((commitCount) => {
+    console.log(commitCount); // eslint-disable-line no-console
+    setBadgeText(commitCount.toString());
+  })
+  .catch((err) => {
+    setBadgeText('X');
+    console.log('ERROR', err); // eslint-disable-line no-console
+  });
