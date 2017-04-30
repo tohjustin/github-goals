8de584ab03fecc7318553ce1d756f4b2d4667f9f
Justin Toh <tohjustin@hotmail.com>
2017-04-30 14:33:16 -0700

    Updated progress bar colors corresponding to each percentage range

--- src/js/modules/theme.js
+++ src/js/modules/theme.js
@@ -2,9 +2,9 @@ export const getColor = (percentage) => {
   if (percentage >= 125) {
     // return '#8F2CFA';
     return '#0366D6';
-  } else if (percentage >= 66) {
+  } else if (percentage >= 100) {
     return '#7ED321';
-  } else if (percentage >= 33) {
+  } else if (percentage >= 50) {
     return '#F5A623';
   }
 
