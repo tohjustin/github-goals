bae913a1469d4db207f7082cb21228a76ae426a1
Justin Toh <tohjustin@hotmail.com>
2017-04-29 23:59:23 -0700

    Made background.js save + load user data into localStorage

--- src/js/background.js
+++ src/js/background.js
@@ -1,22 +1,37 @@
-import * as contributions from './modules/git-contribution';
 import _msg from './modules/msg';
+import * as contributions from './modules/git-contribution';
+import * as store from './modules/store';
 import * as theme from './modules/theme';
 
-/* CONSTANTS & MACROS */
+/* --------------------------------------
+ GLOBAL CONSTANTS & VARS (bad practice T_T)
+-------------------------------------- */
 const UPDATE_INTERVAL = 2 * 60 * 1000;
-const TARGET_CONTRIBUTION_COUNT = 10;
-const GITHUB_USERNAME = 'tohjustin';
+let TARGET_CONTRIBUTION_COUNT = 0;
+let GITHUB_USERNAME = '';
+
+/* --------------------------------------
+ MAIN FUNCTIONS
+-------------------------------------- */
+// Defined as non-arrow function so that we can update the Global variables
+function SYNC_USERDATA() {
+  const temp = store.load();
+  const { targetContributionCount, githubId } = temp;
+  TARGET_CONTRIBUTION_COUNT = targetContributionCount;
+  GITHUB_USERNAME = githubId;
+}
+
 const _UPDATE_BADGE = ({ color, text }) => {
   chrome.browserAction.setBadgeBackgroundColor({ color });
   chrome.browserAction.setBadgeText({ text });
 };
 
 /* updates extension's badge */
-const updateBadge = (username, value) => {
+const UPDATE_BADGE = (githubId, value) => {
   if (value) {
     _UPDATE_BADGE(value);
   } else {
-    contributions.getContributionsOfTheDay(username)
+    contributions.getContributionsOfTheDay(githubId)
       .then((commitCount) => {
         _UPDATE_BADGE({
           color: theme.getColor(Math.round((commitCount / TARGET_CONTRIBUTION_COUNT) * 100)),
@@ -24,24 +39,26 @@ const updateBadge = (username, value) => {
         });
       })
       .catch(() => {
-        console.log('[updateBadge, contributions.getContributionsOfTheDay] ERROR!');
+        console.log('[UPDATE_BADGE, contributions.getContributionsOfTheDay] ERROR!');
       });
   }
 };
 
+/* --------------------------------------
+ GLOBAL CONSTANTS & VARS (bad practice T_T)
+-------------------------------------- */
+SYNC_USERDATA();
+
 /* initialize message-passing module to listen for popup events */
 _msg.init('bg', {
   getUserData: (done) => {
-    done({
-      githubUsername: GITHUB_USERNAME,
-      targetContributionCount: TARGET_CONTRIBUTION_COUNT
-    });
+    done(store.load());
   },
-  updateBadgeText: (done, params) => {
-    updateBadge(GITHUB_USERNAME, params);
+  updateBadgeText: (done, { text, color }) => {
+    UPDATE_BADGE(GITHUB_USERNAME, { text, color });
   }
 });
 
 /* scrap & update commit count periodically + configure it to repeat it periodically */
-updateBadge(GITHUB_USERNAME);
-setInterval(updateBadge(GITHUB_USERNAME), UPDATE_INTERVAL);
+UPDATE_BADGE(GITHUB_USERNAME);
+setInterval(() => { UPDATE_BADGE(GITHUB_USERNAME); }, UPDATE_INTERVAL);
